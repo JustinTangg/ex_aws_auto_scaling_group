dce11298bde85278e6881a1168e59c472270b1cd
Justin Tang <justin.tang@vonage.com>
2018-08-22 09:38:43 -0400

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -11,9 +11,6 @@ defmodule ExAwsAutoScalingGroup.Mixfile do
     ]
   end
 
-  defp elixirc_paths(:test), do: ["lib", "test/support"]
-  defp elixirc_paths(_),     do: ["lib",]
-
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
